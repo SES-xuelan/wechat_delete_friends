59a067f1e53ed47552980ec74dc7e6d4651969cf
Albert Feng <albert_feng@tianqiinfo.com>
2016-04-06 16:09:01 +0800

    增加源码的链接

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 你还在用这种落后的、打扰到别人的方法来清理微信好友么？来，看我口型~~~~你OUT啦！<br />
 今天就让我来给你提供一种不打扰别人、又能知道谁把你删了的方法吧~<br /><br />
 
-本程序是根据 0x5e 大神提供的python版写的PHP版，python版源代码：<a href="https://github.com/0x5e/wechat-deleted-friends">请点击这里</a><br />原理就是新建群组,如果加不进来就是被删好友了(不要在群组里讲话,别人是看不见的)
+本程序是根据 0x5e 大神提供的python版写的PHP版，python版源代码：<a href="https://github.com/0x5e/wechat-deleted-friends">请点击这里</a><br />本程序的源码<a href="https://github.com/SES-xuelan/wechat_delete_friends">在这里</a><br /><br />原理就是新建群组,如果加不进来就是被删好友了(不要在群组里讲话,别人是看不见的)
 <br />
 <h4>目前未解决的问题</h4>
 ①微信限制了接口，接口访问限制会出现【操作太频繁，请稍后再试】，不清楚接口的限制策略是什么,有的同学能用有的不能用<br />
@@ -21,4 +21,4 @@
 	<input type="submit" value="我明白了,开始使用">
 </form>
 </body>
-</html>
+</html></html>
